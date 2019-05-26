84f444306134a2315fc1f7faaf04f067da32b6ef
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2019-05-25 22:10:07 -0400

    Add files via upload

--- LL.cpp
+++ LL.cpp
@@ -15,30 +15,59 @@ class LinkedList {
         LinkedList() {
                 head = NULL;
         }
-        //insert at the start
+        //insert at head
         void insert(int data) {
                 node *temp = new node(data);
                 temp -> next = head;
                 head = temp;
                 }
         void display() {
-                node *current = head;
-                while(current != NULL){
-                    cout << current -> value;
-                    if(current -> next != NULL) {
-                        cout << " -> ";
-                    }
-                    current = current -> next;
+                if(head == NULL) {
+                        cout << "Empty List." << endl;
                 }
-                cout << endl;
+                else {
+                        node *current = head;
+                        while(current != NULL){
+                                cout << current -> value;
+                                if(current -> next != NULL) {
+                                cout << " -> ";
+                                }
+                        current = current -> next;
+                        }
+                        cout << endl;
+                }
+        }
+        //delete at head
+        void Delete() {
+                if(head != NULL) {
+                        node *temp = head;
+                        node *current = temp -> next;
+                        head = current;
+                        delete temp;
+                }
+                else {
+                        cout << "Empty List." << endl;
+                }
+        }
+        void count() {
+                node *current = head;
+                int count = 0;
+                while(current != NULL) {
+                        count++;
+                        current = current -> next;
+                }
+                cout << "Count: " << count << "." << endl;
         }
 };
 int main() {
         LinkedList LL;
-        LL.display();
         LL.insert(6);
         LL.insert(4);
         LL.insert(8);
         LL.display();
+        LL.count();
+        LL.Delete();
+        LL.display();
+        LL.count();
         return 0;
 }

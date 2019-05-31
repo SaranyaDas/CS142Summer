1397ca720f9701dd748f1e6240b81deceee4bea0
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2019-06-01 04:00:14 +0530

    Add files via upload

--- Week1LinkedListOptional.cpp
+++ Week1LinkedListOptional.cpp
@@ -38,10 +38,9 @@ class LinkedList {
 			cout << "NULL" << endl;
 		}
 	}
-	node *removeduplicates(node *head) { //function to delete duplicate-value nodes from sorted linked list
-		node *current = head; //temporary pointer to traverse list, starts from head
-		if(head == NULL || head -> next == NULL) { //cases: empty list or single node
-			return head;
+	node *removeduplicates(node *current) { //function to delete duplicate-value nodes from sorted linked list
+		if(current == NULL || current -> next == NULL) { //cases: empty list or single node
+			return current;
 		}
 		else { //case: non-empty list
 			while(current -> next != NULL) { //as long as pointer does not point to NULL
@@ -53,28 +52,33 @@ class LinkedList {
 					current -> next = temp -> next; //current node now points to node after duplicate node
 					delete temp; //deletes duplicate node
 				}
-				removeduplicates(current -> next); //repeats to check for more duplicate nodes
 			}		
 		}
+	}
+	void *duplicates() { //dummy function
+		removeduplicates(head);
 	}	
-	node *reverse(node *current) { //function to reverse a linked list 
+	node *reverselist(node *current) { //function to reverse a linked list 
 		if(current == NULL || current -> next == NULL) { //i.e., till original current (temporary pointer) points to secondlast node and (original) current -> next = last node AT FIRST
 			head = current; //last node becomes head, head points to NULL now
 			return current; //returns last node (current -> next) AT FIRST
 		}
-		reverse(current -> next); //recursion; repeats till first node (now last) points to NULL (instead of pointing to second node)
+		reverselist(current -> next); //recursion; repeats till first node (now last) points to NULL (instead of pointing to second node)
 		node *temp = current -> next; //current points to secondlast node which points to last node (now head), temp points to last node
 		temp -> next = current; //last node (now head) points to secondlast node now (instead of pointing to NULL)
 		current -> next = NULL; //secondlast node points to NULL (instead of pointing to last node)
 	}
+	void reverse() { //dummy function
+		reverselist(head);
+	}
 };
-void compare(node *current1, node *current2) { //function to compare two linked lists
+void comparelists(node *current1, node *current2) { //function to compare two linked lists
 	if(current1 != NULL && current2 != NULL) { //case: temporary pointers do not point to NULL in either list
 		if(current1 -> data != current2 -> data) { //case: unequal data in corresponding nodes
 			cout << " not identical." << endl;
 		}
 		else { //case: equal data in corresponding nodes
-			compare(current1 -> next, current2 -> next); //pointers traverse lists
+			comparelists(current1 -> next, current2 -> next); //pointers traverse lists
 		}
 	}
 	if(current1 == NULL || current2 == NULL) { //case: corresponding temporary pointers point to NULL in one or both lists  
@@ -86,6 +90,9 @@ void compare(node *current1, node *current2) { //function to compare two linked 
 		}
 	}
 }
+void compare(LinkedList x, LinkedList y) { //dummy function
+	comparelists(x.head, y.head);
+}
 int main() { //driver function
 	LinkedList A;
 	LinkedList B;
@@ -93,6 +100,7 @@ int main() { //driver function
 	LinkedList D;
 	LinkedList E;
 	LinkedList F;
+	E.insert(10);
 	E.insert(9);
 	for(int i = 9; i > 0; i--) {
 		A.insert(i);
@@ -100,6 +108,7 @@ int main() { //driver function
 		C.insert(10 - i);
 		E.insert(i);
 	}
+	E.insert(1);
 	E.insert(1);
 	F.insert(1);
 	cout << "List A display: ";
@@ -115,24 +124,24 @@ int main() { //driver function
 	cout << "List F display: ";
 	F.display();
 	cout << "Lists A and B are";
-	compare(A.head, B.head);
+	compare(A, B);
 	cout << "Lists B and C are";
-	compare(B.head, C.head);
+	compare(B, C);
 	cout << "Lists C and D are";
-	compare(C.head, D.head);
-	E.removeduplicates(E.head);
+	compare(C, D);
+	E.duplicates();
 	cout << "Removed duplicates from sorted Linked List E." << endl;
 	cout << "List E display: ";
 	E.display();
-	E.reverse(E.head);
+	E.reverse();
 	cout << "Reversed Linked List E." << endl;
 	cout << "List E display: ";
 	E.display();
-	D.reverse(D.head);
+	D.reverse();
 	cout << "Reversed Linked List D." << endl;
 	cout << "List D display: ";
 	D.display();
-	F.reverse(F.head);
+	F.reverse();
 	cout << "Reveresed Linked List F." << endl;
 	cout << "List F display: ";
 	F.display();

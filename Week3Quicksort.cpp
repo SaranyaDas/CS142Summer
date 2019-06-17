8d0881e93b1d0cb87e891de5d8d88f26347d8d79
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2019-06-17 06:30:03 -0400

    Add files via upload

--- Week3Quicksort.cpp
+++ Week3Quicksort.cpp
@@ -1,48 +1,48 @@
 #include <iostream>
 using namespace std;
-void swap(int array[], int i, int j) { //function to swap data between two indices
-	int temp = array[i]; //temporary variable to store data at index i
-	array[i] = array[j]; //data at index i becomes equal to data at index j 
-	array[j] = temp; //data at index j becomes equal to temp
+void swap(int array[], int i, int j) {
+	int temp = array[i];
+	array[i] = array[j];
+	array[j] = temp;
 }
-int Partition(int array[], int low, int high) { //function to partition array
-	int pivot = high; //partition happens around index corresponding to pivot's value 
-	int j = low; //index of smaller element
-	for(int i = low; i < high; i++) { //temporary variable to virtually traverse array
-		if(array[i] <= array[pivot]) { //data at index i is less than or equal to data at index corresponding to pivot's value
+int partition(int array[], int low, int high) {
+	int pivot = high;
+	int j = low;
+	for(int i = low; i < high; i++) {
+		if(array[i] <= array[pivot]) {
 			swap(array, i, j);
-			j++; //j increases by 1
+			j++;
 		}
 	}
 	swap(array, pivot, j);
-	return j; //j becomes partitioning index
+	return j;
 }
-void Quicksort(int array[], int low, int high) { //function to sort array such that elements are in ascending order
-	if(low < high) { //exit condition of recursion: low >= high
-		int pi = Partition(array, low, high); //pi = partitioning index = j
-		Quicksort(array, low, pi - 1); //sorts array segment left of pi
-		Quicksort(array, pi + 1, high); //sorts array segment right of pi
+void quicksort(int array[], int low, int high) {
+	if(low < high) {
+		int pi = partition(array, low, high);
+		quicksort(array, low, pi - 1);
+		quicksort(array, pi + 1, high);
 	}
 }
-void Display(int array[], int size) {
-	for(int i = 0; i < size ; i++) {
+void display(int array[], int size) {
+	for(int i = 0; i < size - 1; i++) {
 		cout << array[i] << " ";
 	}
-	cout << endl;
+	cout << array[size - 1] << endl;
 }
 int main() {
 	int size;
 	cout << "Enter size of array: ";
 	cin >> size;
 	int array[size];
-	cout << "Enter the elements one by one:" << endl;
+	cout << "Enter elements of array one by one:" << endl;
 	for(int i = 0; i < size; i++) {
 		cin >> array[i];
 	}
 	cout << "Array: ";
-	Display(array, size);
-	Quicksort(array, 0, size - 1);
+	display(array, size);
 	cout << "Sorted array: ";
-	Display(array, size);
+	quicksort(array, 0, size - 1);
+	display(array, size);
 	return 0;
 }
